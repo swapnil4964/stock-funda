c96c1ac65c70f392a9376581b95569f5f407e617
swapnilh-cognologix <swapnil4964@gmail.com>
2022-10-03 11:42:02 +0530

    stock-funda initial screen design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'stock-funda';
+  ratio1 = 0;
+  ratio2 = 0;
+  cmp = 0;
+
+  getAverage() {
+    return;
+  }
+
+  clearFields() {
+    return;
+  }
 }
